beb64b296c130c1f236c80d804f2d2bb53baa66d
in63119 <in63119@gmail.com>
2022-10-12 16:27:23 +0900

    Fix server index

--- inwallet_server/index.js
+++ inwallet_server/index.js
@@ -9,7 +9,11 @@ const wallet = require("./routes/wallet");
 
 app.use(
   cors({
-    origin: "https://localhost:3000",
+    origin: [
+      "https://localhost:3000",
+      "http://inwallet.ml.s3-website.ap-northeast-2.amazonaws.com/",
+      "https://inwallet.ml",
+    ],
     credentials: true,
   })
 );
@@ -17,6 +21,14 @@ app.use(express.json());
 
 app.use("/wallet", wallet);
 
+app.set("port", PORT);
+
+app.get("/", (req, res) => {
+  res.send("This is IN server!");
+});
+
 app.listen(PORT, () => {
   console.log(`우리 서버 ${PORT}에서 도는 중, 화이팅!`);
 });
+
+module.exports = app;
