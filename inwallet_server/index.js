a280796047bff7c1839ca492315a4d173983eafe
in63119 <in63119@gmail.com>
2022-10-14 23:07:52 +0900

    Fix cors

--- inwallet_server/index.js
+++ inwallet_server/index.js
@@ -4,12 +4,12 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 4000;
 
-let corsOptions = {
-  origin: ["https://inwallet.ml", "https://localhost:3000"],
-  Credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
