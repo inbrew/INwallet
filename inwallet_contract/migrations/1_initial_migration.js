7dd1f300d2f1ddc112e48f0a1302ebb855b01aea
in63119 <in63119@gmail.com>
2022-12-05 22:26:06 +0900

    fix migration

--- inwallet_contract/migrations/1_initial_migration.js
+++ inwallet_contract/migrations/1_initial_migration.js
@@ -1,3 +1,10 @@
-const inProxyContract = artifacts.require("InProxy");
+const inProxy = artifacts.require("InProxy");
 
-module.exports = async function (deployer) {};
+module.exports = async function (deployer) {
+  await deployer.deploy(inProxy);
+
+  const inProxyContract = await inProxy.deployed();
+
+  // makeData 해야함
+  // make...
+};
