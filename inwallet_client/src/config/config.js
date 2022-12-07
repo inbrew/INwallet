c2de55ee0b9423c337ff03b7f338b1c8b490c6c4
in63119 <in63119@gmail.com>
2022-12-07 21:34:01 +0900

    Create config

---
+++ inwallet_client/src/config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  server: {
+    localURL: "http://localhost:4000",
+    deployURL: "https://inserver.ml",
+  },
+  binance: {
+    rpcURL: "https://bsc-testnet.public.blastapi.io",
+  },
+  ethereum: {
+    rpcURL: "https://goerli.infura.io/v3/fa408e18052a47b18c82a59f8b54c1c6",
+  },
+  klaytn: {
+    rpcURL: "https://api.baobab.klaytn.net:8651/",
+  },
+  polygon: {
+    rpcURL:
+      "https://polygon-mumbai.infura.io/v3/fa408e18052a47b18c82a59f8b54c1c6",
+  },
+};
