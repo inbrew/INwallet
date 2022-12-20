53675219e46af171286606ad515860db78cc850a
in63119 <in63119@gmail.com>
2022-12-20 20:02:21 +0900

    Fix migration

--- inwallet_contract/migrations/1_initial_migration.js
+++ inwallet_contract/migrations/1_initial_migration.js
@@ -5,13 +5,13 @@ const { makeInProxy, makeInNFT } = require("../data/makeJson");
 
 const { sha3 } = require("../setting");
 
-// module.exports = async function (deployer) {
-//   await deployer.deploy(inNFT);
-//   const inNFTContract = await inNFT.deployed();
-//   //   await deployer.deploy(inProxy, inNFTContract.address, sha3);
-//   //   const inProxyContract = await inProxy.deployed();
-//   //   await inNFTContract.setContractAddress(inProxyContract.address);
+module.exports = async function (deployer) {
+  await deployer.deploy(inNFT);
+  const inNFTContract = await inNFT.deployed();
+  //   await deployer.deploy(inProxy, inNFTContract.address, sha3);
+  //   const inProxyContract = await inProxy.deployed();
+  //   await inNFTContract.setContractAddress(inProxyContract.address);
 
-//   makeInNFT(inNFTContract.address);
-//   //   makeInProxy(inProxyContract.address);
-// };
+  makeInNFT(inNFTContract.address);
+  //   makeInProxy(inProxyContract.address);
+};
